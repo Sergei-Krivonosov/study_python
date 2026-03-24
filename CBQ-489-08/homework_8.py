d1f4d1c8e5709c4a8bd2f550f216a40d2c239993
Sergei Krivonosov <ksva690@gmail.com>
2026-03-24 16:31:13 +0200

    CBQ-489-08

---
+++ CBQ-489-08/homework_8.py
@@ -0,0 +1,43 @@
+#Task 1
+
+class BankAccount:
+
+    def __init__(self, name, balance):
+        self._name = name
+        self._balance = balance
+
+    def deposit(self, amount):
+        self._balance += amount
+
+    def withdraw(self, amount):
+        if self._balance < amount:
+            print('Недостаточно средств!')
+        else:
+            self._balance -= amount
+
+    def get_balance(self):
+        return self._balance
+
+account = BankAccount('Maria', 1000)
+account.deposit(900)
+account.withdraw(100)
+print(account.get_balance())
+
+
+
+
+#Task2
+
+
+class OverdraftAccount(BankAccount):
+
+    def withdraw(self, amount):
+        if self._balance <= amount:
+            self._balance -= amount
+            print('Недостаточно средств!')
+
+jack_account = OverdraftAccount('jack', 0)
+jack_account.withdraw(200)
+jack_account.withdraw(100)
+jack_account.withdraw(100)
+print(jack_account.get_balance())
